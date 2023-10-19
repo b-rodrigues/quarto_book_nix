2ac9ff1271d192fcecab5591caa3cf4163f67b79
Bruno Rodrigues <bruno@brodrigues.co>
2023-10-19 15:11:49 +0200

    rendering locally succeeds

--- define_env.R
+++ define_env.R
@@ -3,7 +3,16 @@ library(rix)
 rix(r_ver = "4.3.1",
     r_pkgs = c("quarto"),
     system_pkgs = "quarto",
-    tex_pkgs = c("amsmath"),
+    tex_pkgs = c(
+      "amsmath",
+      "fvextra",
+      "environ",
+      "fontawesome5",
+      "orcidlink",
+      "pdfcol",
+      "tcolorbox",
+      "tikzfill"
+    ),
     ide = "other",
     shell_hook = "",
     project_path = ".",
